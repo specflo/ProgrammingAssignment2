2a9d11a6145425c69582cc4845d8f4ec8acfe97a
specflo <sergiodeluca2004@hotmail.com>
2017-10-21 21:21:46 +1100

    cachematrix.R exercise coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-lll
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
 
 ## Write a short comment describing this function
 
+
+        makeCacheMatrix <- function(x = matrix()) {
+                inv <- NULL
+                set <- function(y) {
+                        x <<- y
+                        inv <<- NULL
+                }
+                get <- function() x
+                setinv <- function(solve) inv <<- solve(x)
+                getinv <- function() inv
+                list(set = set, get = get,
+                     setinv = setinv,
+                     getinv = getinv)
+        }
+        
+        
+        
+        
+        
+
+
+## Write a short comment describing this function
+## m substituted by inv, my name for the inverse matrix, and mean -->> solve 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+        
+        
+        
+        
+        
+        }
